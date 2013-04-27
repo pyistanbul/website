6be304de2820e3c6f7726fb798a3e8a643c3bad9
fatiherikli <fatiherikli@gmail.com>
2013-04-28 02:34:17 +0300

    Customized radpress templates

--- web/pyist/urls.py
+++ web/pyist/urls.py
@@ -1,17 +1,27 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-from django.views.generic import TemplateView
+
+from radpress.views import ArticleListView, ArticleDetailView
+
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+
+    # apps
     url(r'^people/', include('people.urls', namespace="people")),
     url(r'^jobs/', include('jobs.urls', namespace="jobs")),
     url(r'^presentations/', include('presentations.urls',
                                     namespace="presentations")),
-    url(r'^blog/', include('radpress.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+
+    # third party apps
+    url(r'^$', ArticleListView.as_view(template_name="index.html"),
+        name='home'),
+    url(r'^blog/(?P<slug>[-\w]+)$',view=ArticleDetailView.as_view(),
+        name='radpress-article-detail'),
+
+    # admin
+    url(r'^admin/',
+        include(admin.site.urls)),
 )
