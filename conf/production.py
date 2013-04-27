733f1eab9a9703f97c3fe65edf12c9aafb960d61
fatiherikli <fatiherikli@gmail.com>
2013-04-28 01:46:28 +0300

    Added production settings

---
+++ conf/production.py
@@ -0,0 +1,16 @@
+"""
+This file should be linked as settings_local.py in the settings directory.
+"""
+
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(__file__), '../data/db.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
