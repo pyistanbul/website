f4a24f5ed0b4f0c91f36c112b402ad4d8b10adba
fatiherikli <fatiherikli@gmail.com>
2013-04-28 03:00:59 +0300

    Fixed the success message of job create view

--- web/jobs/views.py
+++ web/jobs/views.py
@@ -21,6 +21,9 @@ class CreateJobView(CreateView):
     success_message = 'İlanınız başarıyle eklendi. Editörler tarafından ' \
                       'onaylandıktan sonra sitede yayınlanacaktır.'
 
-    def get_success_url(self):
+    def form_valid(self, form):
         messages.success(self.request, self.success_message)
+        return super(CreateJobView, self).form_valid(form)
+
+    def get_success_url(self):
         return reverse("jobs:index")
