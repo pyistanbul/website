4517304eb4d1d4f40e07306a0c1d468799abc2e7
Cihan Okyay <cihan.okyay@metglobal.com>
2014-06-22 22:56:51 +0300

    fix datetime RunTimeWarning

--- jobs/managers.py
+++ jobs/managers.py
@@ -1,12 +1,13 @@
 import datetime
 
 from django.db import models
+from django.utils import timezone
 
 
 class JobsManager(models.Manager):
 
     def active(self):
-        now = datetime.datetime.now()
+        now = timezone.now()
         three_months_ago = now - datetime.timedelta(weeks=3 * 4)
         return super(JobsManager, self).get_query_set().filter(
             date_created__gte=three_months_ago)
