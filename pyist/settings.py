40932b7e7938b14b7827822e1788ad07b6fd9e8f
Berker Peksag <berker.peksag@gmail.com>
2014-11-01 13:09:23 +0200

    Activate django.contrib.flatpages.
    Fixes #8.

--- pyist/settings.py
+++ pyist/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'djangospam.cookie.middleware.SpamCookieMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'pyist.urls'
@@ -70,6 +71,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.flatpages',
 
     'south',
     'django_gravatar',
