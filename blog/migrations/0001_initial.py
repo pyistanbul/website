ebe4616a99f4c3aef37ee566ea0ca66b51df89a8
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-03-04 22:43:14 +0200

    migration added

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Post'
+        db.create_table(u'blog_post', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255)),
+            ('description', self.gf('markitup.fields.MarkupField')(no_rendered_field=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('is_published', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('_description_rendered', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal(u'blog', ['Post'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Post'
+        db.delete_table(u'blog_post')
+
+
+    models = {
+        u'blog.post': {
+            'Meta': {'ordering': "['-created_at']", 'object_name': 'Post'},
+            '_description_rendered': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('markitup.fields.MarkupField', [], {'no_rendered_field': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['blog']
