5042bd43a83d5d6f389c2a69ae8d50a94d6305fd
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-05-08 11:12:30 +0300

    migration conflict

---
+++ people/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('people', '0002_auto_20150311_1220'),
+        ('people', '0003_auto_20150507_2307'),
+    ]
+
+    operations = [
+    ]
