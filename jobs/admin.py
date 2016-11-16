ec50b3ee44fcec1252663b2a66bfe8f304381189
kenansubasi <kenansubasiceng@gmail.com>
2016-11-16 18:52:31 +0300

    fixes #52: Updated jobs admin panel display.

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,4 +3,18 @@ from django.contrib import admin
 from jobs.models import Job
 
 
-admin.site.register(Job)
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        ('Temel Bilgiler', {
+            'fields': ('title', 'company', 'location'),
+        }),
+        ('Detay Bilgiler', {
+            'fields': ('url', 'date_created', 'description'),
+        }),
+    )
+
+    list_display = ('title', 'company')
+    list_filter = ('company',)
+    search_fields = ('title', 'company')
