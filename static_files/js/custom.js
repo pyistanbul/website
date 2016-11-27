b3f789ae9c1580035dced38a566e4c2c9bf22fc9
Kamyar Ghasemlou <k.ghasemlou@gmail.com>
2016-11-27 17:41:18 +0300

    fix custom class name

--- static_files/js/custom.js
+++ static_files/js/custom.js
@@ -3,14 +3,14 @@ $(function() {
     /* Handler show/hide on scroll */
     $(window).scroll(function() {
         if ($(document).scrollTop() > 100) {
-            $('.footer-container').addClass("show");
+            $('.footer-container').addClass("kg-show");
         } else {
-            $('.footer-container').removeClass("show");
+            $('.footer-container').removeClass("kg-show");
         }
     });
 
     /* Handle onClick show/hide */
     $('.footer-container').click(function(){
-        $('.footer-container').toggleClass("show");
+        $('.footer-container').toggleClass("kg-show");
     })
 })
