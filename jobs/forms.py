12394f891a5fe35ddf4b16eea28c526935c3fdd7
Cihan Okyay <okyaycihan@gmail.com>
2018-11-30 13:51:41 +0300

    removed jobs app

--- jobs/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Job
-
-
-class JobForm(forms.ModelForm):
-    class Meta:
-        model = Job
-        fields = ('title', 'company', 'url', 'description', 'location')
