d78021e71e6c8ebc38b59468dea0b35c5ff2ec7c
umitdincel <dincelumit@gmail.com>
2018-12-18 00:09:58 +0300

    design fixes for people page

--- people/views.py
+++ people/views.py
@@ -1,17 +1,15 @@
-from django.contrib import messages
 from django.urls import reverse
-from django.views.generic import CreateView, ListView
+from django.views.generic import ListView
 
 from people.forms import PersonForm
 from people.models import Person
-from pyist.mixins import SuccessMessageMixin
 
 
 class PeopleView(ListView):
     queryset = Person.objects.active()
 
 
-class CreatePeopleView(SuccessMessageMixin, CreateView):
+class PeopleView(ListView):
     model = Person
     form_class = PersonForm
     success_message = 'Kişi başarıyla eklendi.'
