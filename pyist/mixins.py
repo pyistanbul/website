6d81307455413310ea8cbeb2a36206ab28667e7b
Bahattin Cinic <bahattin@adphorus.com>
2018-12-18 23:38:09 +0300

    review changes

--- pyist/mixins.py
+++ pyist/mixins.py
@@ -1,9 +1,15 @@
 from django.contrib import messages
+from django.core.exceptions import ImproperlyConfigured
 
 
-class SuccessMessageMixin(object):
+class SuccessMessageMixin:
     success_message = None
 
     def form_valid(self, form):
+        if not self.success_message:
+            raise ImproperlyConfigured(
+                'No message to show. Provide a success_message.'
+            )
+
         messages.success(self.request, self.success_message)
         return super().form_valid(form)
