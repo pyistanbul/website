946575df4124e123665d02a2c7e164ae9c0c7e9b
Bahattin Çiniç <bahattincinic@gmail.com>
2018-12-19 03:24:41 +0300

    Added SuccessMessageMixin (#74)
    Closes #73

--- people/views.py
+++ people/views.py
@@ -4,20 +4,17 @@ from django.views.generic import CreateView, ListView
 
 from people.forms import PersonForm
 from people.models import Person
+from pyist.mixins import SuccessMessageMixin
 
 
 class PeopleView(ListView):
     queryset = Person.objects.active()
 
 
-class CreatePeopleView(CreateView):
+class CreatePeopleView(SuccessMessageMixin, CreateView):
     model = Person
     form_class = PersonForm
     success_message = 'Kişi başarıyla eklendi.'
-
-    def form_valid(self, form):
-        messages.success(self.request, self.success_message)
-        return super(CreatePeopleView, self).form_valid(form)
 
     def get_success_url(self):
         return reverse("people:index")
